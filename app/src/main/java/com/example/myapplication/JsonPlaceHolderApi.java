f5d91fe630ad518f47e0b343f525395100e81a46
MateuszKMechaniktg <mateusz.kubica@uczen.mechaniktg.pl>
2025-12-05 12:12:03 +0100

    Polaczenie json z aplikacja w MainActivity.java

--- app/src/main/java/com/example/myapplication/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/myapplication/JsonPlaceHolderApi.java
@@ -5,7 +5,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-public interface JavaPlaceHolderApi {
+public interface JsonPlaceHolderApi {
     @GET("pytania")
     public Call<List<Pytanie>> getPytania();
 }
